879c3fff8adb2d1477fe05206f660a3283c2ac42
Christophe Maillot <christophe.maillot@gmail.com>
2025-12-29 21:40:26 +0100

    added make change script

---
+++ make_changes.sh
@@ -0,0 +1,11 @@
+echo "modif 1" > file1.txt
+git add file1.txt
+git commit -m "first edit of push"
+
+echo "modif 2" > file1.txt
+git add file1.txt
+git commit -m "second edit for this file"
+
+echo "modif 3" > file1.txt
+git add file1.txt
+git commit -m "edit number three"
